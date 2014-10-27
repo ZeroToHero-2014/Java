4120e6fd6a22c67ed4bda630220564b99346a96b
Mihai7 <mihai_niculae93@yahoo.com>
2014-10-27 17:55:39 +0200

    Ziua 2

---
+++  exercises/Ziua 1/Project1/src/main/java/ro/teamnet/zerotohero/threads/thread_creation/ClasaMostenireThread.java
@@ -0,0 +1,11 @@
+package ro.teamnet.zerotohero.threads.thread_creation;
+
+/**
+ * Created by user on 10/27/2014.
+ */
+public class ClasaMostenireThread extends Thread {
+    @Override
+    public void run() {
+        System.out.println("Sunt in clasa care mosteneste Thread");
+    }
+}

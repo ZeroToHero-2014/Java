34f73e24f4a58a934723b7ca1a5511619f6986a0
GabiR <gabi.rtr@gmail.com>
2014-10-28 13:02:11 +0200

    annotations

---
+++  exercises/src/main/java/ro/teamnet/zerotohero/reflection/model/Employee.java
@@ -0,0 +1,17 @@
+package ro.teamnet.zerotohero.reflection.model;
+
+import ro.teamnet.zerotohero.reflection.api.Column;
+import ro.teamnet.zerotohero.reflection.api.Table;
+
+/**
+ * Created by Juvie on 28.10.2014.
+ */
+
+@Table(name = "EMPLOYEE")
+public class Employee {
+    @Column(col = "NAME")
+    String name;
+    @Column(col = "SALARY")
+    float salary;
+
+}

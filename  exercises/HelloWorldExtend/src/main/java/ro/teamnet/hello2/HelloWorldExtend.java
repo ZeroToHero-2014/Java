aabd038cf2b37af02f31f5c5a4039e36c8bcbcd4
Constantinescu <denisa.constantinescu92@gmail.com>
2014-10-29 17:46:35 +0200

    Marvel1 Extend

---
+++  exercises/HelloWorldExtend/src/main/java/ro/teamnet/hello2/HelloWorldExtend.java
@@ -0,0 +1,18 @@
+package ro.teamnet.hello2;
+
+import ro.teamnet.hello.HelloWorld;
+
+/**
+ * Created by user on 10/29/2014.
+ */
+public class HelloWorldExtend {
+    public HelloWorldExtend() {
+    }
+
+    public void extendSayHello(){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+        System.out.println("The new Hello World");
+    }
+
+}

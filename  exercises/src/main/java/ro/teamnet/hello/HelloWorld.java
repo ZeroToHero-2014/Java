d432929f156b4dc034c3ec825a0078e119535db6
andreidobre <dobreandy@yahoo.com>
2014-10-29 11:32:40 +0200

    luni_marti_miercuri

---  exercises/src/main/java/ro/teamnet/hello/HelloWorld.java
+++  exercises/src/main/java/ro/teamnet/hello/HelloWorld.java
@@ -1,5 +1,8 @@
 package ro.teamnet.hello;
 
+
+import org.apache.log4j.Logger;
+
 /**
  * Created by andrei.dobre on 29.10.2014.
  */
@@ -9,7 +12,12 @@ public class HelloWorld {
      * method for saying hello
      */
     public void sayHello(){
+
         System.out.println("Hello World!");
+
+        logger.debug("DEBUG -> Enters in sayHello() method from HelloWorld");
+        logger.info("INFO -> Enters in returnHelloKey from HelloWorld");
+
     }
 
     /**
@@ -25,5 +33,6 @@ public class HelloWorld {
         helloWorld.sayHello();
     }
 
+    static final Logger logger = Logger.getLogger(HelloWorld.class.getName());
 
 }

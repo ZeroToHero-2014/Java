026b38d76208fa9d274a35f834fa0214ad312074
iancubogdan <iancubogdan91@yahoo.com>
2014-10-30 09:21:10 +0200

    Workshop Maven

---
+++  exercises/HelloWorldExtend/src/main/java/ro/teamnet/hello2/HelloWorldExtend.java
@@ -0,0 +1,22 @@
+package ro.teamnet.hello2;
+
+import ro.teamnet.hello.HelloWorld;
+
+/**
+ * Created by Bogdan on 10/29/2014.
+ */
+public class HelloWorldExtend
+{
+
+    public HelloWorldExtend()
+    {
+    }
+
+    public void extendSayHello(){
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+        System.out.println("The new Hello World");
+    }
+
+}
+
---
+++  exercises/HelloWorldMain/src/test/java/ro/teamnet/hello/HelloWorldTest.java
@@ -0,0 +1,25 @@
+package ro.teamnet.hello;
+
+import org.junit.Test;
+
+/**
+ * Created by Bogdan on 10/29/2014.
+ */
+public class HelloWorldTest
+{
+    @Test
+    public void testSayHello()
+    {
+        HelloWorld helloWorld = new HelloWorld();
+        helloWorld.sayHello();
+    }
+
+    @Test
+    public void testReturnHelloKey()
+    {
+        HelloWorld helloWorld = new HelloWorld();
+        System.out.println("From Test: " + helloWorld.returnHelloKey());
+        assert helloWorld.returnHelloKey().equals("HelloKey");
+    }
+
+}

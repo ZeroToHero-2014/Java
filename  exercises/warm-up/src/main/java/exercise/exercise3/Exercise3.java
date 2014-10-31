9c1e2ce985026b3185a4ced537f8dffaa7d28a25
ovi ovi <craiu.ovidiu93@yahoo.com>
2014-10-31 20:26:39 +0200

    Workshop Collection 1(full)

---  exercises/warm-up/src/main/java/exercise/exercise3/Exercise3.java
+++  exercises/warm-up/src/main/java/exercise/exercise3/Exercise3.java
@@ -1,6 +1,6 @@
 package exercise.exercise3;
 
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by Radu.Hoaghe on 10/28/2014.
@@ -22,21 +22,42 @@ public class Exercise3 {
 
     // List containing some elements that need to be added into the Set
     private List<String> listToAdd;
-
+    Set<String> st= new HashSet<String>();
+    Set<String> st1 = new LinkedHashSet<String>();
+    Set<String> st2= new TreeSet<String>();
     public Exercise3(List l) {
         listToAdd = l;
+
+        st.addAll(listToAdd);
+
+        st1.addAll(listToAdd);
+
+        st2.addAll(listToAdd);
     }
 
     public void addElementsToSets(){
 
         System.out.println("The elements that will be added to the Sets: ");
         // TODO Exercise #3 a) Check the content of the elements you will add into the Set
-
+        System.out.println(st);
+        System.out.println(st1);
+        System.out.println(st2);
         // TODO Exercise #3 b) add the elements from listToAdd to the Sets
 
         // TODO Exercise #3 c) Check the content of the Sets
 
         // TODO Exercise #3 d) Add to the TreeSet two elements that already exist in the Set
         // TODO Exercise #3 d) and print again the TreeSet. What do you see?
+    }
+
+    public static void main(String[] args) {
+        List<String> li= new ArrayList<String>();
+        li.add("mare arar nebuni msds fds");
+        li.add("mare gfdgfdgdfarar nefgfdbuni gfdgmsds fds");
+        li.add("mare ararfgfdgfdg nebuni msds fds");
+        Exercise3 exe = new Exercise3(li);
+        exe.addElementsToSets();
+
+
     }
 }

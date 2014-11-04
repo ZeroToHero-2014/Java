fa2852a7706d51e8c6bf585d4a11aa0c945236b8
sMoKeSz <iuly_zep_pelin@yahoo.com>
2014-11-04 23:55:24 +0200

    zth-app-1.1 add OracleDataSource + pstm

---  exercises/zth-app/src/main/java/ro/teamnet/zth/utils/DatabaseManager.java
+++  exercises/zth-app/src/main/java/ro/teamnet/zth/utils/DatabaseManager.java
@@ -1,6 +1,8 @@
 package ro.teamnet.zth.utils;
 
 
+import oracle.jdbc.pool.*;
+
 import java.sql.*;
 import java.util.HashMap;
 
@@ -8,13 +10,30 @@ public class DatabaseManager {
 
 
     public static Connection getConnection(String username, String password) {
+//        Connection con = null;
+//
+//        try {
+//            con = DriverManager.getConnection("jdbc:oracle:thin:@demo.teamnet.ro:1521:orcl",
+//                    username,
+//                    password);
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+//        return con;
+
+        OracleDataSource ds;
         Connection con = null;
-
         try {
-            con = DriverManager.getConnection("jdbc:oracle:thin:@10.6.33.102.:1521:orcl",
-                    username,
-                    password);
-
+            ds = new OracleDataSource();
+            ds.setDriverType("thin");
+            ds.setServerName("demo.teamnet.ro");
+            //  ds.setURL("jdbc:oracle:thin:@demo.teamnet.ro:1521:orcl");
+            ds.setPortNumber(1521);
+            ds.setDatabaseName("orcl");
+            ds.setUser(username);
+            ds.setPassword(password);
+            con = ds.getConnection();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -22,11 +41,36 @@ public class DatabaseManager {
     }
 
     public static void checkConnection(Connection con) {
+//
+//        Statement statment = null;
+//        try {
+//            statment = con.createStatement();
+//            ResultSet rs = statment.executeQuery("SELECT SYSDATE FROM DUAL");
+//            if (rs.next()) {
+//                Date currentDate = rs.getDate(1);
+//                System.out.println("Current Date from Oracle is :" + currentDate);
+//
+//            }
+//            rs.close();
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        } finally {
+//            try {
+//                statment.close();
+//            } catch (SQLException e) {
+//                e.printStackTrace();
+//            }
+//
+//
+//        }
 
-        Statement statment = null;
+        PreparedStatement stm = null;
+
+
         try {
-            statment = con.createStatement();
-            ResultSet rs = statment.executeQuery("SELECT SYSDATE FROM DUAL");
+            String SQL = "SELECT SYSDATE FROM DUAL";
+            stm = con.prepareStatement(SQL);
+            ResultSet rs = stm.executeQuery();
             if (rs.next()) {
                 Date currentDate = rs.getDate(1);
                 System.out.println("Current Date from Oracle is :" + currentDate);
@@ -37,57 +81,82 @@ public class DatabaseManager {
             e.printStackTrace();
         } finally {
             try {
-                statment.close();
+                if (stm != null) {
+                    stm.close();
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
-
         }
+
+
     }
 
 
     public static void create(Connection con, String tableName, HashMap<String, String> columnData) {
 
         Statement statement = null;
-            try {
-                statement=con.createStatement();
-                String createTableString= "CREATE TABLE "+tableName+"(";
+        try {
+            statement = con.createStatement();
+            String createTableString = "CREATE TABLE " + tableName + "(";
 
-                StringBuilder sqlStatment = new StringBuilder();
-                sqlStatment.append(createTableString);
-                Integer columnsCount=columnData.keySet().size();
+            StringBuilder sqlStatment = new StringBuilder();
+            sqlStatment.append(createTableString);
+            Integer columnsCount = columnData.keySet().size();
 
-                for(String columnName : columnData.keySet()){
+            for (String columnName : columnData.keySet()) {
 
-                    columnsCount--;
-                    String columnString = columnName +" "+columnData.get(columnName)+(columnsCount !=0 ? " , " : ")");
-                    sqlStatment.append(columnString);
-                }
-
-                statement.executeUpdate(sqlStatment.toString());
-                statement.close();
-                System.out.println("Created  table "+tableName+" in database...");
-            } catch (SQLException e) {
-                e.printStackTrace();
+                columnsCount--;
+                String columnString = columnName + " " + columnData.get(columnName) + (columnsCount != 0 ? " , " : ")");
+                sqlStatment.append(columnString);
             }
+
+            statement.executeUpdate(sqlStatment.toString());
+            statement.close();
+            System.out.println("Created  table " + tableName + " in database...");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 
 
     public static void drop(Connection con, String tableName) {
-        Statement statement = null;
+//        Statement statement = null;
+//
+//        try {
+//            statement = con.createStatement();
+//            String dropTableStatment = "DROP TABLE " + tableName;
+//            statement.executeUpdate(dropTableStatment);
+//            statement.close();
+//            System.out.println("Deleted table " + tableName);
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+
+        PreparedStatement stm = null;
 
         try {
-            statement=con.createStatement();
-            String dropTableStatment = "DROP TABLE " + tableName;
-            statement.executeUpdate(dropTableStatment);
-            statement.close();
-            System.out.println("Deleted table "+tableName);
+            String SQL = "DROP TABLE "+ tableName;
+            stm = con.prepareStatement(SQL);
+            stm.executeUpdate();
+            stm.close();
+            System.out.println("Deleted table " + tableName);
         } catch (SQLException e) {
             e.printStackTrace();
-        }
+        }finally {
+            try {
+                if (stm != null) {
+                    stm.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
 
+        }
     }
 
     public static void closeConnection(Connection con) {
